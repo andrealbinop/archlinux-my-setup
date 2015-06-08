93eafb5f8b3dabdfb98ca0ca32a44da6cf421489
Andre Albino Pereira <andre@softplan.com.br>
2015-06-07 22:54:39 -0300

    initial commit, tries to run post_setup automatically on first boot

--- setup.sh
+++ setup.sh
@@ -2,26 +2,28 @@
 
 ANSIBLE_HOSTS=ansible/resources/hosts
 SCRIPTS_DIR=$(readlink -m "$(dirname $0)")
-CHROOT_SCRIPTS_DIR=/$(basename $SCRIPTS_DIR)
-INSTALL_DIR=/mnt
+export INSTALL_DIR=/$(basename $SCRIPTS_DIR)
+CHROOT_BASE_DIR=/mnt
 
 echo 'starting installation...'
 
-if [ ! -f $INSTALL_DIR/bin/bash ]; then
+if [ ! -f $CHROOT_BASE_DIR/bin/bash ]; then
   # runs ansible pre_install script
   echo 'configuring disks and installing base system...'
   ansible-playbook -i $ANSIBLE_HOSTS $SCRIPTS_DIR/ansible/pre_setup.yml
 fi
 
 if [  $? == 0 ]; then
-  cp -R $SCRIPTS_DIR $INSTALL_DIR$CHROOT_SCRIPTS_DIR
+  rm -rf $CHROOT_BASE_DIR$INSTALL_DIR
+  cp -R $SCRIPTS_DIR $CHROOT_BASE_DIR$INSTALL_DIR
   # runs ansible install script while chrooted
   echo 'configuring base system...'
-  arch-chroot $INSTALL_DIR ansible-playbook -i $CHROOT_SCRIPTS_DIR/$ANSIBLE_HOSTS $CHROOT_SCRIPTS_DIR/ansible/setup.yml
+  arch-chroot $CHROOT_BASE_DIR ansible-playbook -i $INSTALL_DIR/$ANSIBLE_HOSTS $INSTALL_DIR/ansible/setup.yml
 
   if [  $? == 0 ]; then
     echo 'installation finished successfully, rebooting...'
-    umount --recursive $INSTALL_DIR
+    umount --recursive $CHROOT_BASE_DIR
     reboot
+    exit 0
   fi
 fi
